354b736057fcbf1a6a547fde78286ceeb593aba6
Jeanette Mui <jeanettemui101@gmail.com>
2019-11-12 19:14:42 -0800

    Tracker + Board

---
+++ Assets/Scripts/AnimalManager.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimalManager : MonoBehaviour
+{
+    // For updating text
+    public Text animal;
+    public Text day;
+    public Text spaces;
+
+    // Animals start with 7 energy, 0 Experience
+    int energy = 7;
+    int experience = 0;
+    int swol = 1;
+    int moveCounter = 0;
+    int spacesMoved = 0;
+    int currDay = 1;
+    string currAnimal;
+
+    // Initialize variables
+    void Start()
+    {
+        currAnimal = animal.text;
+        animal.text = currAnimal + "Swol: " + swol + " Energy: " + energy + " Exp: " + experience;
+    }
+
+    // Button Function
+    public void Feed() 
+    {
+        energy += 5;
+        UpdateText();
+    }
+
+    void UpdateText()
+    {
+        animal.text = currAnimal + "Swol: " + swol + " Energy: " + energy + " Exp: " + experience;
+        spaces.text = "Spaces Moved: " + spacesMoved;
+    }
+     // Energy -> Exp -> Noah
+
+// When Energy goes to 0, Can't get Swol
+// Noah gets 12 moves a day and must return to his home at the end of the day
+// Noah can come feed animals, Energy increase by 5 when on same space as animal
+// Animals lose 1 energy every 2 moves
+//                 gain 1 exp if they have energy
+//                 gain 1 swol for every 3 experience
+// If animals goes to sleep w/o energy, -1 swol
+
+    // button functions to update when move button is pressed
+    public void Move() 
+    {
+        spacesMoved++;
+        if(moveCounter != 1)
+            moveCounter++;
+        else
+            moveCounter = 0;
+
+        // Gain exp if animal has energy every 2 moves
+        if(energy > 0 && moveCounter == 1) 
+        {
+            energy--;
+            
+            if(experience < 3) 
+                experience++;
+            if(experience == 3)
+            {
+                experience = 0;
+                swol++;
+            }
+        } 
+        // might need to change this so it doesnt run 4 times
+        if(spacesMoved == 12) {
+            EndDay();
+            spacesMoved = 0;
+        }
+        UpdateText();
+    }
+
+    public void EndDay()
+    {
+        // lose swol if animal has no energy
+        if(energy == 0 && swol > 1)
+            swol--;
+        
+        // update current day
+        currDay++;
+        day.text = "Day: " + currDay;
+        spaces.text = "Spaces Moved: " + spacesMoved;
+    }
+
+}
---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+    // Reference to the Prefab. Drag a Prefab into this field in the Inspector.
+    public GameObject grid;
+    public GameObject ladder;
+    public GameObject border;
+    public GameObject animal;
+
+    /*
+        0 - moveable
+        1 - wall
+        2 - ladder
+        3 - animals
+        5 - moveable + grid (temp)
+     */
+    int[,] board = new int[5, 9] 
+    { 
+        {1, 1, 1, 1, 1, 1, 1, 1, 1}, 
+        {2, 0 ,3, 0, 2, 0, 5, 3, 2}, 
+        {2, 3 ,0, 5, 2, 5, 0, 5, 2}, 
+        {2, 0 ,5, 0, 2, 0, 3, 0, 2}, 
+        {1, 1, 1, 1, 1, 1, 1, 1, 1},  
+    };
+
+    // Set up board using an array
+    void Start()
+    {
+        // board column len
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                //if i,j == 1, put a wall
+                if(board[i,j] == 1) 
+                {
+                    // Instantiate at position and rotation.
+                    Instantiate(border, new Vector3(j + 1, i - 1, 0.5f), Quaternion.Euler(-90, 0, 0));
+                    Debug.Log(border.transform.rotation.x);
+                }
+                // put a ladder
+                else if(board[i,j] == 2) 
+                {
+                    Instantiate(ladder, new Vector3(j + 1, i - 1, 0.5f), Quaternion.Euler(-90, 0, 0));
+                }
+
+                // put an animal
+                else if(board[i,j] == 3) 
+                {
+                    Instantiate(animal, new Vector3(j + 1, i - 1, 0.5f), Quaternion.Euler(-90, 0, 0));
+                }
+                
+                // put a grid square
+                else if(board[i,j] == 5) 
+                {
+                    Instantiate(grid, new Vector3(j + 1, i - 1, 0.5f), Quaternion.Euler(-90, 0, 0));
+                }
+            }
+        }
+        
+    }
+
+    void Update()
+    {
+        // Player can only move down if on a ladder
+
+        // button compenent to update ?
+
+    }
+}
